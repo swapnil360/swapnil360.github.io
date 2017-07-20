199d8c627053c2fdb041d99ecf9c36f23583de37
Swapnil Chitnis <swapnilchitnis360@gmail.com>
2017-07-20 11:51:43 +0530

    FULL REWAMP [BETA]

--- js/app.js
+++ js/app.js
@@ -1,4 +1,44 @@
-$(document).ready(function (){
-  var liftoffTime = new Date(1995, 6-1, 8);
-  $("#age").countdown({until: liftoffTime});
+$(document).ready(function(){
+  // $('.home-navigation').find('ul').attr("style", "max-width: 0")
+  //
+  // $('.home-navigation').find('ul').animate({
+  //   "max-width": "50em",
+  // }, 400, function() {
+  //   $('ul').attr('style', 'overflow: inherit');
+  //   $('.nav-page-title').animate({
+  //     "bottom": "120px",
+  //     "opacity": "1",
+  //     "transition": "none"
+  //   }, 1)
+  // });
+
+  $('#home').click(function() {
+    $(location).attr('href', 'index.html');
+  })
+
+  $('#portfolio').click(function() {
+    $(location).attr('href', 'portfolio.html');
+  })
+
+  $('#skills').click(function() {
+    $(location).attr('href', 'skills.html');
+  })
+
+  $('#experience').click(function() {
+    $(location).attr('href', 'experience.html');
+  })
+
+  $('#contact').click(function() {
+    $(location).attr('href', 'contact.html');
+  })
+
+  $('.professional-experience').find('h6').click(function() {
+    $('.professional-experience').removeClass('professional-experience--expanded');
+    $(this).parent().addClass('professional-experience--expanded');
+  })
+
+  $('.fa-times').click(function(event) {
+    console.log($(this).parent('.professional-experience'));
+    $(this).parent().removeClass('professional-experience--expanded');
+  });
 });
